230721cd144771188a530df2cf1781884c603c32
rayen445 <nesstinebacc@gmail.com>
2024-04-11 17:03:12 +0200

    commit

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
-#include "sort.h"
 
 /**
- * quick_sort - Sorts an array of integers in ascending order using the Quick sort algorithm
+ * print_array - Prints an array of integers
  *
- * @array: The array to be sorted
+ * @array: The array to be printed
  * @size: Number of elements in the array
  */
-void quick_sort(int *array, size_t size)
+void print_array(const int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+    size_t i;
 
-    /* TODO: Implement Quick sort algorithm */
+    for (i = 0; i < size; ++i)
+    {
+        printf("%d", array[i]);
+        if (i < size - 1)
+            printf(", ");
+    }
+    printf("\n");
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    quick_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
 }

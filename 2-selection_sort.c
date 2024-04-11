cf3f08004c6339e4fe41b37ba4a2988f4b42aafc
rayen445 <nesstinebacc@gmail.com>
2024-04-11 16:57:12 +0200

    commit

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,27 +9,27 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index;
-    int tmp;
+	size_t i, j, min_index;
+	int tmp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_index = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
 
-        if (min_index != i)
-        {
-            tmp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = tmp;
-            print_array(array, size);
-        }
-    }
+		if (min_index != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = tmp;
+			print_array(array, size);
+		}
+	}
 }
